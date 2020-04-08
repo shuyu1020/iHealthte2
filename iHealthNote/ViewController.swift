a782fd16b02f289280c90ac297be2545531e03d7
shuyu1020 <m10711009@gapps.ntust.edu.tw>
2020-04-08 22:39:51 +0800

    add navigation structure

---
+++ iHealthNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iHealthNote
+//
+//  Created by ntust02 on 2020/4/7.
+//  Copyright © 2020 朱詩婷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
